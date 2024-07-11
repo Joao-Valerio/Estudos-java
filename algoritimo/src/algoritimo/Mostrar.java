8402fd2d83240244d5030f9b33da7389778c0d16
João Valério Santana <151927936+ohomiebrabo@users.noreply.github.com>
2024-07-11 14:14:54 -0300

    First commit

---
+++ algoritimo/src/algoritimo/Mostrar.java
@@ -0,0 +1,19 @@
+package algoritimo;
+
+import java.util.Scanner;
+
+public class Mostrar {
+	public static void main(String[] args) {
+		Scanner t = new Scanner(System.in);
+		int[] v = new int[20];
+		for (int c = 0; c < v.length; c++) {
+			System.out.println("Informe o " + (c + 1) + "º Valor do Vetor");
+			v[c] = t.nextInt();
+		}
+		Selection.SelectionSort(v);
+		System.out.println("Vetor ordenado:");
+		for (int num : v) {
+			System.out.print(num + " ");
+		}
+	}
+}
