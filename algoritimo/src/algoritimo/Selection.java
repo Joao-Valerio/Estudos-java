dee72c48654f8266e5630a2ac69b69ab100e4c96
João Valério Santana <151927936+ohomiebrabo@users.noreply.github.com>
2024-07-11 17:12:51 -0300

    Second

--- algoritimo/src/algoritimo/Selection.java
+++ algoritimo/src/algoritimo/Selection.java
@@ -3,11 +3,17 @@ package algoritimo;
 public class Selection {
 	public static void SelectionSort(int[] v) {
 		int x = v.length;
+		boolean c;
 		for (int z = 0; z < x - 1; z++) {
+			c=true;
 			int minIndex = z;
 			for (int t = z + 1; t < x; t++) {
 				if (v[t] < v[minIndex]) {
 					minIndex = t;
+					c=false;
+				}
+				if(c) {
+					break;
 				}
 			}
 			int temp = v[minIndex];
