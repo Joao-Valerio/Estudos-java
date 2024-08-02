5eae92ecc95ed1e67fab870c26d04837dbe9f9f4
João Valério Santana <151927936+ohomiebrabo@users.noreply.github.com>
2024-08-02 17:19:53 -0300

    fix error

--- algoritimo/src/algoritimo/Selection.java
+++ algoritimo/src/algoritimo/Selection.java
@@ -3,18 +3,15 @@ package algoritimo;
 public class Selection {
 	public static void SelectionSort(int[] v) {
 		int x = v.length;
-		boolean c;
+
 		for (int z = 0; z < x - 1; z++) {
-			c=true;
 			int minIndex = z;
 			for (int t = z + 1; t < x; t++) {
 				if (v[t] < v[minIndex]) {
 					minIndex = t;
-					c=false;
+
 				}
-				if(c) {
-					break;
-				}
+
 			}
 			int temp = v[minIndex];
 			v[minIndex] = v[z];
